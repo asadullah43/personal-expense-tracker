c18393feab8e51e1a815b7dca5656df2e2547be0
asadUllah43 <asadu043@gmail.com>
2024-05-28 00:06:18 +0300

    added logic for chart date and sums of amount

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/transaction.dart';
+import 'package:expense_tracker/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -20,18 +21,32 @@ class Chart extends StatelessWidget {
           totalSum += recentTransaction[i].amount;
         }
       }
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    }).reversed.toList();
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
-    return const Card(
+    return Card(
       elevation: 5,
-      margin: EdgeInsets.all(20),
+      margin: const EdgeInsets.all(20),
       child: Row(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.spaceAround ,
+        children: groupedTransactionValues.map((data) {
+          return ChartBar(
+              label: (data['day'] as String),
+              spendingAmount: (data['amount'] as double),
+              spendingPctOfTotal: totalSpending==0? 0.0 : (data['amount'] as double) / totalSpending);
+        }).toList(),
       ),
     );
   }
